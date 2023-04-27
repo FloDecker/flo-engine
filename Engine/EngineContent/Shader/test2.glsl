ea3e5c9dd272a522b96297ea7d70c0db2026f63b
Florian Decker <flo12212@gmail.com>
2023-04-27 19:15:12 +0200

    quick fix

--- Engine/EngineContent/Shader/test2.glsl
+++ Engine/EngineContent/Shader/test2.glsl
@@ -1,11 +1,4 @@
 [vertex]
-#version 330 core
-layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aNormal;
-layout (location = 2) in vec2 aUV;
-uniform mat4 mMatrix;
-uniform mat4 vMatrix;
-uniform mat4 pMatrix;
 
 void main() {
     vec4 vertexCamSpace =vMatrix * mMatrix * vec4(aPos, 1.0);
@@ -13,8 +6,6 @@ void main() {
 }
 
 [fragment]
-#version 330 core
-out vec4 FragColor;
 
 void main() {
     FragColor = vec4(1.0f);
